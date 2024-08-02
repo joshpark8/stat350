3ddb626788849166a2e9ba459bd3996d7a5b0ad0
Josh Park <84344359+joshpark8@users.noreply.github.com>
2024-08-02 01:50:14 -0400

    add ch8, 9

--- homeworks/ca9/ca9c.R
+++ homeworks/ca9/ca9c.R
@@ -5,19 +5,4 @@ df <- subset(CreditDataSummer2024_cleaned, CreditDataSummer2024_cleaned$duration
 # 1a
 fit <- lm(lamount ~ duration, data = df)
 sample <- data.frame(duration = 7.25)
-predict(fit, sample, interval = "prediction")
-
-# 1b
-fit <- lm(resids ~ duration, data = df)
-df$resids <- fit$residuals
-predict(fit, sample, interval = "prediction")
-
-# 2a
-fit <- lm(lamount ~ duration, data = df)
-sample <- data.frame(duration = 7.25)
-predict(fit, sample, interval = "confidence", level = 0.99)
-
-# 3a
-fit <- lm(lamount ~ duration, data = df)
-sample <- data.frame(duration = 7.25)
-predict(fit, sample, interval = "prediction", level = 0.99)
+predict(fit, sample, interval = 
